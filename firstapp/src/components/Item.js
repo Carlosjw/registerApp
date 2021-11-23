cc7cb3e02761eccc3437677d87259eb8b7059d5f
unknown <carlos.ti.infor@gmail.com>
2021-11-23 12:51:36 -0300

    lessonPropsAdvanced

--- firstapp/src/components/Item.js
+++ firstapp/src/components/Item.js
@@ -1,10 +1,24 @@
-function Item(props) {
+import PropTypes from "prop-types";
+
+function Item({ marca, anolancamento }) {
   return (
     <>
-      <li>{props.marca}</li>
-      <p>Teste</p>
+      <li>
+        {marca} - {anolancamento}
+      </li>
     </>
   );
 }
 
+// validando a nível de programação (ideal para deixar para outros programadores)
+Item.prototypes = {
+  marca: PropTypes.string.isRequired,
+  anolancamento: PropTypes.number,
+};
+
+// caso não seja fornecido nada, será preenchido pelo defaultprops
+Item.defaultProps = {
+  marca: "Faltou a marca",
+  anolancamento: 0,
+};
 export default Item;
--- firstapp/src/components/List.js
+++ firstapp/src/components/List.js
@@ -5,9 +5,11 @@ function List() {
     <>
       <h1>Minha Lista</h1>
       <ul>
-        <Item marca="Ferrari" />
-        <Item marca="Bugatti" />
-        <Item marca="Lamborghinni" />
+        <Item marca="Ferrari" anolancamento={1985} />
+        <Item marca="Bugatti" anolancamento={1964} />
+        <Item marca="Lamborghinni" anolancamento={2001} />
+        <Item marca="Chevrolet" anolancamento={1984} />
+        <Item />
       </ul>
     </>
   );
