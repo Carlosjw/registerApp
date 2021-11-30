cb948d0b77e7f26c1a0049e3f3f193d8c60f384d
unknown <carlos.ti.infor@gmail.com>
2021-11-30 11:11:49 -0300

    updateApp

--- firstapp/src/components/Condicional.js
+++ firstapp/src/components/Condicional.js
@@ -1,19 +1,37 @@
 import { useState } from "react";
 
 function Condicional() {
+  const [email, setEmail] = useState();
+  const [userEmail, setUserEmail] = useState();
+
   function enviarEmail(e) {
     e.preventDefault();
-    console.log("Testando...");
+    setUserEmail(email);
+    console.log(userEmail);
+  }
+
+  function limparEmail() {
+    setUserEmail("");
   }
   return (
     <div>
       <h2>Cadastre seu e-mail</h2>
       <form>
-        <input type="email" placeholder="Digite seu e-mail..." />
+        <input
+          type="email"
+          placeholder="Digite seu e-mail..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </form>
-      <button type="submit" nClick={enviarEmail}>
+      <button type="submit" onClick={enviarEmail}>
         Enviar e-mail
       </button>
+      {userEmail && (
+        <div>
+          <p>O e-mail do usuário é: {userEmail}</p>
+          <button onClick={limparEmail}>Limpar e-mail</button>
+        </div>
+      )}
     </div>
   );
 }
